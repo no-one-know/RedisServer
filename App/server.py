276e4b0de538c44a9e151ea0f918973c218b9e53
chetan <bagra200chetan@gmail.com>
2024-10-23 22:59:36 +0530

    enabling multiple clients to connect to the server

--- App/server.py
+++ App/server.py
@@ -1,20 +1,27 @@
-import socket  # noqa: F401
+import socket  
+import threading
+
+def handle_client(connection, address):
+    print(f"Accepted connection from {address}")
+    while(True):
+        data = connection.recv(1024)
+        if not data:
+            break
+        print(f"Received: {data} from address: {address}")
+        connection.sendall(b"+PONG\r\n")
+    connection.close()
 
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(("localhost", 6379))
-    server_socket.listen(2)  # 5 is the maximum number of clients that can be queued
+    server_socket.listen(3)  
     print("Server is listening for connections")
-    connection, address = server_socket.accept()  # wait for client
-    print(f"Connection from {address}")
-    while(True):
-        data = connection.recv(1024)
-        if not data:
-            break
-        print(f"Received: {data}")
-        connection.sendall(b"+PONG\r\n")
-    connection.close()
+    # Now we will wait for a client to connect and assign it to a new thread to achieve concurrency
+    while True:
+        connection, address = server_socket.accept()  # wait for client
+        thread = threading.Thread(target=handle_client, args=(connection, address))
+        thread.start()
 
 if __name__ == "__main__":
     main()
